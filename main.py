e7927d3231558ecaee00ae6bbae771e240a37879
bradley-dev777 <hu.yu@wcs-g.com>
2024-11-25 16:25:31 -0700

    UPDATE

--- main.py
+++ main.py
@@ -1,6 +1,62 @@
+def on_up_pressed():
+    if not (playerSprite.tile_kind_at(TileDirection.CENTER, assets.tile("""
+        myTile
+    """)) or (playerSprite.tile_kind_at(TileDirection.CENTER, assets.tile("""
+        myTile2
+    """)) or playerSprite.tile_kind_at(TileDirection.CENTER, assets.tile("""
+        myTile3
+    """)))):
+        tiles.place_on_tile(playerSprite,
+            tiles.get_tile_location(playerSprite.tilemap_location().column,
+                playerSprite.tilemap_location().row - 1))
+controller.up.on_event(ControllerButtonEvent.PRESSED, on_up_pressed)
+
 def on_left_pressed():
-    playerSprite.x += -1
+    if not (playerSprite.tile_kind_at(TileDirection.CENTER, assets.tile("""
+        myTile
+    """)) or (playerSprite.tile_kind_at(TileDirection.CENTER, assets.tile("""
+        myTile0
+    """)) or playerSprite.tile_kind_at(TileDirection.CENTER, assets.tile("""
+        myTile5
+    """)))):
+        tiles.place_on_tile(playerSprite,
+            tiles.get_tile_location(playerSprite.tilemap_location().column - 1,
+                playerSprite.tilemap_location().row))
 controller.left.on_event(ControllerButtonEvent.PRESSED, on_left_pressed)
+
+def on_overlap_tile(sprite, location):
+    game.splash("LEVEL FINISHED! NEXT LEVEL!")
+scene.on_overlap_tile(SpriteKind.player,
+    assets.tile("""
+        myTile16
+    """),
+    on_overlap_tile)
+
+def on_right_pressed():
+    if not (playerSprite.tile_kind_at(TileDirection.CENTER, assets.tile("""
+        myTile1
+    """)) or (playerSprite.tile_kind_at(TileDirection.CENTER, assets.tile("""
+        myTile2
+    """)) or playerSprite.tile_kind_at(TileDirection.CENTER, assets.tile("""
+        myTile7
+    """)))):
+        tiles.place_on_tile(playerSprite,
+            tiles.get_tile_location(playerSprite.tilemap_location().column + 1,
+                playerSprite.tilemap_location().row))
+controller.right.on_event(ControllerButtonEvent.PRESSED, on_right_pressed)
+
+def on_down_pressed():
+    if not (playerSprite.tile_kind_at(TileDirection.CENTER, assets.tile("""
+        myTile1
+    """)) or (playerSprite.tile_kind_at(TileDirection.CENTER, assets.tile("""
+        myTile0
+    """)) or playerSprite.tile_kind_at(TileDirection.CENTER, assets.tile("""
+        myTile6
+    """)))):
+        tiles.place_on_tile(playerSprite,
+            tiles.get_tile_location(playerSprite.tilemap_location().column,
+                playerSprite.tilemap_location().row + 1))
+controller.down.on_event(ControllerButtonEvent.PRESSED, on_down_pressed)
 
 playerSprite: Sprite = None
 tiles.set_current_tilemap(tilemap("""
--- main.ts
+++ main.ts
@@ -1,6 +1,24 @@
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (!(playerSprite.tileKindAt(TileDirection.Center, assets.tile`myTile`) || (playerSprite.tileKindAt(TileDirection.Center, assets.tile`myTile2`) || playerSprite.tileKindAt(TileDirection.Center, assets.tile`myTile3`)))) {
+        tiles.placeOnTile(playerSprite, tiles.getTileLocation(playerSprite.tilemapLocation().column, playerSprite.tilemapLocation().row - 1))
+    }
+})
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     if (!(playerSprite.tileKindAt(TileDirection.Center, assets.tile`myTile`) || (playerSprite.tileKindAt(TileDirection.Center, assets.tile`myTile0`) || playerSprite.tileKindAt(TileDirection.Center, assets.tile`myTile5`)))) {
-        playerSprite.x += -1
+        tiles.placeOnTile(playerSprite, tiles.getTileLocation(playerSprite.tilemapLocation().column - 1, playerSprite.tilemapLocation().row))
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile16`, function (sprite, location) {
+    game.splash("LEVEL FINISHED! NEXT LEVEL!")
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (!(playerSprite.tileKindAt(TileDirection.Center, assets.tile`myTile1`) || (playerSprite.tileKindAt(TileDirection.Center, assets.tile`myTile2`) || playerSprite.tileKindAt(TileDirection.Center, assets.tile`myTile7`)))) {
+        tiles.placeOnTile(playerSprite, tiles.getTileLocation(playerSprite.tilemapLocation().column + 1, playerSprite.tilemapLocation().row))
+    }
+})
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (!(playerSprite.tileKindAt(TileDirection.Center, assets.tile`myTile1`) || (playerSprite.tileKindAt(TileDirection.Center, assets.tile`myTile0`) || playerSprite.tileKindAt(TileDirection.Center, assets.tile`myTile6`)))) {
+        tiles.placeOnTile(playerSprite, tiles.getTileLocation(playerSprite.tilemapLocation().column, playerSprite.tilemapLocation().row + 1))
     }
 })
 let playerSprite: Sprite = null
